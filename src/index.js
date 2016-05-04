93d019c2d3c3388e4e158dac11a60864203d3660
justinvdm <justinvdm.0.1.0@gmail.com>
2016-05-04 12:53:08 +0200

    Add test for stubbed out POST /users/ endpoint

--- src/index.js
+++ src/index.js
@@ -5,8 +5,10 @@ const routes = require('src/routes');
 const errors = require('src/middleware/api/errors');
 
 
-const app = new Koa()
-  .use(logger());
+const env = process.env.NODE_ENV || 'dev';
+const app = new Koa();
+
+if (env !== 'test') app.use(logger());
 
 errors
   .forEach(error => app.use(error));
---
+++ tests/routes/users.test.js
@@ -0,0 +1,17 @@
+const request = require('supertest');
+const app = require('src');
+
+
+describe("/users/", () => {
+  describe("POST /users/", () => {
+    it("should respond with a 501", done => {
+      request(app.listen())
+        .post('/users/', {})
+        .expect(501)
+        .expect({
+          type: 'not_implemented',
+        })
+        .end(done);
+    });
+  });
+});
