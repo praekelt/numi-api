941a04e94577ed65d7c0c79571dc135baf9a967d
justinvdm <justinvdm.0.1.0@gmail.com>
2016-05-06 12:54:49 +0200

    Flesh out /projects/ routes

---
+++ src/routes/projects.js
@@ -0,0 +1,17 @@
+const _ = require('koa-route');
+const { project: schemas } = require('schemas').definitions;
+const { projects } = require('src/api');
+const { create, read, update, patch } = require('src/middleware/api/methods');
+
+
+module.exports = [
+  _.post('/projects/', create(schemas.new, projects.create)),
+  _.get('/projects/', read(projects.getAll, () => ({
+    page: 1,
+    per_page: 100
+  }))),
+  _.get('/projects/:id', read(projects.get)),
+  _.get('/projects/:id/channels/', read(projects.getChannels)),
+  _.put('/projects/:id', update(schemas.project, projects.update)),
+  _.patch('/projects/:id', patch(schemas.project, projects.patch))
+];
