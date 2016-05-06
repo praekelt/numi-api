99bfd2149fdcb56c80d30a7e26b016b363d904be
justinvdm <justinvdm.0.1.0@gmail.com>
2016-05-06 12:24:12 +0200

    Update schema index to reference all schemas

--- schemas/index.js
+++ schemas/index.js
@@ -4,7 +4,7 @@ const deref = require('json-schema-deref-sync');
 const jsonPatch = require('./json-patch');
 
 
-function readConfig(path) {
+function readSchema(path) {
   return read(resolve(__dirname, path));
 }
 
@@ -12,10 +12,49 @@ function readConfig(path) {
 module.exports = deref({
   definitions: {
     json_patch: jsonPatch,
-    config: readConfig('./config.yml'),
+    config: readSchema('./config.yml'),
+    release: readSchema('./release.yml'),
     user: {
-      user: readConfig('./user/user.yml'),
-      new: readConfig('./user/new.yml')
+      user: readSchema('./user/user.yml'),
+      new: readSchema('./user/new.yml'),
+      summary: readSchema('./user/summary.yml'),
+      password_change: readSchema('./user/password-change.yml'),
+      password_reset: readSchema('./user/password-reset.yml'),
+      password_confirmation: readSchema('./user/password-confirmation.yml')
+    },
+    permission: {
+      permission: readSchema('./permission/permission.yml'),
+      dialogues_read: readSchema('./permission/dialogues_read.yml'),
+      dialogues_write: readSchema('./permission/dialogues_write.yml'),
+      project_admin: readSchema('./permission/project_admin.yml'),
+      project_dialogues_read: readSchema(
+        './permission/project_dialogues_read.yml'),
+      project_dialogues_write: readSchema(
+          './permission/project_dialogues_write.yml')
+    },
+    project: {
+      project: readSchema('./project/project.yml'),
+      summary: readSchema('./project/summary.yml')
+    },
+    dialogue: {
+      dialogue: readSchema('./dialogue/dialogue.yml'),
+      summary: readSchema('./dialogue/summary.yml'),
+      sequence: readSchema('./dialogue/sequence.yml'),
+      block: readSchema('./dialogue/block.yml'),
+      symbol: readSchema('./dialogue/symbol.yml')
+    },
+    revision: {
+      revision: readSchema('./revision/revision.yml'),
+      revert: readSchema('./revision/revert.yml'),
+      edit: readSchema('./revision/edit.yml')
+    },
+    channel: {
+      channel: readSchema('./channel/channel.yml'),
+      summary: readSchema('./channel/summary.yml')
+    },
+    provider: {
+      provider: readSchema('./provider/provider.yml'),
+      summary: readSchema('./provider/summary.yml')
     }
   }
 });
