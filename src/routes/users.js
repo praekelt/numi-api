2e1270a9a52d23538092958652bc637423e65c2b
justinvdm <justinvdm.0.1.0@gmail.com>
2016-05-06 12:27:57 +0200

    Flesh out /users/ routes

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,9 +1,16 @@
 const _ = require('koa-route');
-const schemas = require('schemas');
+const { user: schemas } = require('schemas').definitions;
 const { users } = require('src/api');
-const { create } = require('src/middleware/api/methods');
+const { create, read, update, patch } = require('src/middleware/api/methods');
 
 
 module.exports = [
-  _.post('/users/', create(schemas.definitions.user.new, users.create))
+  _.post('/users/', create(schemas.new, users.create)),
+  _.get('/users/', read(users.getAll, () => ({
+    page: 1,
+    per_page: 100
+  }))),
+  _.get('/users/:id', read(users.get)),
+  _.put('/users/:id', update(schemas.user, users.update)),
+  _.patch('/users/:id', patch(schemas.user, users.patch))
 ];
