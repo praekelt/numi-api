c4706f40d5d09789db2015a3044ca068d05064f6
justinvdm <justinvdm.0.1.0@gmail.com>
2016-06-06 22:42:38 +0200

    Add channel permissions

--- src/contexts/index.js
+++ src/contexts/index.js
@@ -1,8 +1,10 @@
+const channel = require('src/contexts/channel');
 const project = require('src/contexts/project');
 const organization = require('src/contexts/organization');
 
 
 module.exports = {
   project,
-  organization
+  organization,
+  channel
 };
--- src/permissions/index.js
+++ src/permissions/index.js
@@ -1,8 +1,10 @@
 const permission = require('src/permissions/permission');
 const project = require('src/permissions/project');
+const channel = require('src/permissions/channel');
 
 
 module.exports = {
   permission,
-  project
+  project,
+  channel
 };
