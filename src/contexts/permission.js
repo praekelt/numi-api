bbefed47e03196bd47e1829d90fdbcdcde56c90e
justinvdm <justinvdm.0.1.0@gmail.com>
2016-06-08 23:06:12 +0200

    Implement contexts.permission.removeAccess()

--- src/contexts/permission.js
+++ src/contexts/permission.js
@@ -1,8 +1,23 @@
-const { NotImplementedError } = require('src/errors');
+const find = require('lodash/find');
 const project = require('src/contexts/project');
+const authApi = require('src/auth');
+const { authConf } = require('src/auth-utils');
+const { NotImplementedError } = require('src/errors');
 
 
 function createAccess(teamId, permission) {
+  return access(permission);
+}
+
+
+function removeAccess(teamId, id, {auth}) {
+  return authApi.teams.get(teamId, {conf: authConf(auth)})
+    .then(({permissions}) => find(permissions, {id}))
+    .then(access);
+}
+
+
+function access(permission) {
   const {
     type,
     object_id: id
@@ -17,11 +32,6 @@ function createAccess(teamId, permission) {
     default:
       return Promise.reject(new NotImplementedError());
   }
-}
-
-
-function removeAccess() {
-  throw new NotImplementedError();
 }
 
 
--- tests/contexts/permission.test.js
+++ tests/contexts/permission.test.js
@@ -2,6 +2,8 @@ const { NotImplementedError } = require('src/errors');
 const { expect, fail } = require('chai');
 const { sandbox } = require('sinon');
 const { permission } = require('src/contexts');
+const { authConf } = require('src/auth-utils');
+const authApi = require('src/auth');
 const projects = require('src/projects');
 
 
@@ -13,6 +15,7 @@ describe('contexts.permission', () => {
   afterEach(() => {
     this.sandbox.restore();
   });
+
 
   describe('createAccess', () => {
     it('should get the contexts for project permission types', () => {
@@ -53,9 +56,77 @@ describe('contexts.permission', () => {
   });
 
   describe('removeAccess', () => {
-    it('should throw a NotImplementedError', () => {
-      expect(() => permission.removeAccess())
-        .to.throw(NotImplementedError);
+    it('should get the contexts for project permission types', () => {
+      const expected = {
+        projectId: 23,
+        organizationId: 21
+      };
+
+      const auth = {
+        type: 'token',
+        token: 'abc'
+      };
+
+      this.sandbox.stub(projects, 'get')
+        .withArgs(23)
+        .returns(Promise.resolve({
+          id: 23,
+          organization_id: 21
+        }));
+
+      this.sandbox.stub(authApi.teams, 'get')
+        .withArgs(7, {conf: authConf(auth)})
+        .returns(Promise.resolve({
+          permissions: [{
+            id: 1,
+            type: 'project:admin',
+            object_id: 23
+          }, {
+            id: 2,
+            type: 'project:read',
+            object_id: 23
+          }, {
+            id: 3,
+            type: 'project:write',
+            object_id: 23
+          }]
+        }));
+
+        return Promise.all([1, 2, 3]
+          .map(id => permission.removeAccess(7, id, {auth})))
+          .then(res => expect(res).to.deep.equal([
+            expected,
+            expected,
+            expected
+          ]));
+    });
+
+    it('should throw a NotImplementedError for unsupported permission types',
+    () => {
+      const auth = {
+        type: 'token',
+        token: 'abc'
+      };
+
+      this.sandbox.stub(projects, 'get')
+        .withArgs(23)
+        .returns(Promise.resolve({
+          id: 23,
+          organization_id: 21
+        }));
+
+      this.sandbox.stub(authApi.teams, 'get')
+        .withArgs(7, {conf: authConf(auth)})
+        .returns(Promise.resolve({
+          permissions: [{
+            id: 1,
+            type: 'unsupported',
+            object_id: 23
+          }]
+        }));
+
+        return permission.removeAccess(7, 1, {auth})
+          .then(fail, e => expect(e).to.be.instanceof(NotImplementedError));
     });
   });
 });
