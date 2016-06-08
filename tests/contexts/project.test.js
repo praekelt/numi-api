0bef540799470bed20d281beb94ee01450469b16
justinvdm <justinvdm.0.1.0@gmail.com>
2016-06-08 22:17:41 +0200

    Use sandbox for stubbing in contexts.project tests

--- tests/contexts/project.test.js
+++ tests/contexts/project.test.js
@@ -1,15 +1,21 @@
 const { expect } = require('chai');
-const { stub, restore } = require('sinon');
+const { sandbox } = require('sinon');
 const { project } = require('src/contexts');
 const projects = require('src/projects');
 
 
 describe('contexts.project', () => {
-  afterEach(() => { restore(); });
+  beforeEach(() => {
+    this.sandbox = sandbox.create();
+  });
+
+  afterEach(() => {
+    this.sandbox.restore();
+  });
 
   describe('access', () => {
     it("should get the project's access context", () => {
-      stub(projects, 'get')
+      this.sandbox.stub(projects, 'get')
         .withArgs(23)
         .returns(Promise.resolve({
           id: 23,
