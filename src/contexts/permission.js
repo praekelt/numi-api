4beb3994eb4d48cd30800aeef6155d46bfbf48c4
justinvdm <justinvdm.0.1.0@gmail.com>
2016-06-08 22:21:35 +0200

    Implement contexts.permission.createAccess()

--- src/contexts/permission.js
+++ src/contexts/permission.js
@@ -1,8 +1,22 @@
 const { NotImplementedError } = require('src/errors');
+const project = require('src/contexts/project');
 
 
-function createAccess() {
-  throw new NotImplementedError();
+function createAccess(teamId, permission) {
+  const {
+    type,
+    object_id: id
+  } = permission;
+
+  switch (type) {
+    case 'project:admin':
+    case 'project:read':
+    case 'project:write':
+      return project.access(id);
+
+    default:
+      return Promise.reject(new NotImplementedError());
+  }
 }
 
 
--- tests/contexts/permission.test.js
+++ tests/contexts/permission.test.js
@@ -1,13 +1,54 @@
-const { expect } = require('chai');
-const { permission } = require('src/contexts');
 const { NotImplementedError } = require('src/errors');
+const { expect, fail } = require('chai');
+const { sandbox } = require('sinon');
+const { permission } = require('src/contexts');
+const projects = require('src/projects');
 
 
 describe('contexts.permission', () => {
+  beforeEach(() => {
+    this.sandbox = sandbox.create();
+  });
+
+  afterEach(() => {
+    this.sandbox.restore();
+  });
+
   describe('createAccess', () => {
-    it('should throw a NotImplementedError', () => {
-      expect(() => permission.createAccess())
-        .to.throw(NotImplementedError);
+    it('should get the contexts for project permission types', () => {
+      const expected = {
+        projectId: 23,
+        organizationId: 21
+      };
+
+      this.sandbox.stub(projects, 'get')
+        .withArgs(23)
+        .returns(Promise.resolve({
+          id: 23,
+          organization_id: 21
+        }));
+
+        return Promise.all([
+            'project:admin',
+            'project:read',
+            'project:write'
+          ].map(type => permission.createAccess(7, {
+            type,
+            object_id: 23
+          })))
+          .then(res => expect(res).to.deep.equal([
+            expected,
+            expected,
+            expected
+          ]));
+    });
+
+    it('should reject with NotImplementedError for unsupported types', () => {
+      return permission.createAccess(7, {
+          type: 'unsupported',
+          object_id: 23
+        })
+        .then(fail, e => expect(e).to.be.instanceof(NotImplementedError));
     });
   });
 
