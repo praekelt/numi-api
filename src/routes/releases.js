8a263c807baedbde979e5a3f1161777f7a75d02a
justinvdm <justinvdm.0.1.0@gmail.com>
2016-06-13 13:32:10 +0200

    Eslint fixes

--- src/routes/releases.js
+++ src/routes/releases.js
@@ -1,7 +1,7 @@
 const _ = require('koa-route');
 const { release: schema } = require('schemas').definitions;
 const { releases } = require('src/api');
-const { create, list, read } = require('src/middlewares/methods');
+const { create, list } = require('src/middlewares/methods');
 const { release: permissions } = require('src/permissions');
 const contexts = require('src/contexts');
 
