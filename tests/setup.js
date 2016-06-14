1acbf0e382e1de0690f058648829257aa9450c94
justinvdm <justinvdm.0.1.0@gmail.com>
2016-06-14 14:23:08 +0200

    Add chai-shallow-deep-equal as a dev dependency

--- tests/setup.js
+++ tests/setup.js
@@ -1,6 +1,8 @@
+const chai = require('chai');
 const replay = require('replay');
 const { join } = require('path');
 
 
+chai.use(require('chai-shallow-deep-equal'));
 replay.reset('localhost');
 replay.fixtures = join(__dirname, '.http-fixtures');
