5368d79d16d91e3c2db254008b6bd9c34a3c7bd7
justinvdm <justinvdm.0.1.0@gmail.com>
2016-06-15 23:13:36 +0200

    Minor improvements to contexts.dialogue.access test

--- tests/contexts/dialogue.test.js
+++ tests/contexts/dialogue.test.js
@@ -25,24 +25,24 @@ describe("contexts.dialogue", () => {
   describe("access", () => {
     it("should get the dialogue's access context", () => {
       this.sandbox.stub(projects, 'get')
-        .withArgs(1)
+        .withArgs('2')
         .returns(fakeProjectsResult(fakeProject({
-          id: 1,
-          organization_id: 1
+          id: '2',
+          organization_id: '3'
         })));
 
       this.sandbox.stub(dialogues, 'get')
-        .withArgs(1)
+        .withArgs('1')
         .returns(fakeDialoguesResult(fakeDialogue({
-          id: 1,
-          project_id: 1
+          id: '1',
+          project_id: '2'
         })));
 
-      return dialogue.access(1)
+      return dialogue.access('1')
         .then(res => expect(res).to.deep.equal({
-          dialogueId: 1,
-          projectId: 1,
-          organizationId: 1
+          dialogueId: '1',
+          projectId: '2',
+          organizationId: '3'
         }));
     });
   });
